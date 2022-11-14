1d8227ee7fa05a98db5ee030bfa7bbe62e5b0782
JaroStancik <stancikj@gmail.com>
2022-11-14 17:19:42 +0100

    Added Segmented Buttons.

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from "@angular/core";
+import { SegmentChangeEventDetail } from "@ionic/angular";
 
 import { Place } from "../place.model";
 import { PlacesService } from "../places.service";
@@ -17,5 +18,9 @@ export class DiscoverPage implements OnInit {
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
     this.listedLoadedPlaces = this.loadedPlaces.slice(1);
+  }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);
   }
 }
